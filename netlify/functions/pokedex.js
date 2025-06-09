d8ed376c91ad20ef7e69e0484ee15fa21a100846
meethatkar <meethatkar000@gmail.com>
2025-06-09 16:35:12 +0530

    added costumize apis

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,10 +1,16 @@
-const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+const url = 'https://pokeapi.co/api/v2/pokedex/';
+// const url = process.env.API_URL;
 
-exports.handler = async function(){
-    const APIresponse = await fetch(url);
+exports.handler = async function(event, context){
+    const eventBody = JSON.parse(event.body);
+    const APIresponse = await fetch(url+eventBody.region);
     const data = await APIresponse.json();
+    // console.log(event);
     return {
         statusCode: 200,
-        body: JSON.stringify(data),
+        body: JSON.stringify({
+            pokemon: data.pokemon_entries,
+            // pokemon: data,
+        })
     }
 }
